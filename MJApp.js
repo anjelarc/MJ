7571fe7de31b55c99e53c43de7f9d119fd5f7aac
anjelarc <anjela>
2016-02-06 00:56:15 -0500

    Display home page
    Display home page if user is not logged in

--- MJApp.js
+++ MJApp.js
@@ -1,15 +1,22 @@
 ItemList = new Mongo.Collection('blogs');
-if (Meteor.isClient) {
-  Template.body.helpers({
-  	blogs:function(){
-      	return ItemList.find();
-      }
-      
-  });  
 
-  Template.registerHelper('formatDate', function(createdAt) {
-  return moment(createdAt).format('MM-DD-YYYY');
-});
+if (Meteor.isClient) {
+	Template.home.helpers({
+	  	blogs:function(){
+	      	return ItemList.find();
+	      }
+      
+	});  
+
+	Template.login.events({
+	    'submit form': function(){
+	    	//todo
+	    }
+	});
+
+	Template.registerHelper('formatDate', function(createdAt) {
+  		return moment(createdAt).format('MM-DD-YYYY');
+	});
 }
 
 if (Meteor.isServer) {
