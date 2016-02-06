8b14aa78ef4236d9e0206a8a421da9efb39d3eae
anjelarc <anjela>
2016-02-05 23:58:40 -0500

    Populate and display list item
    Created the collection in MongoDB,
    Retrieve data from db,
    Populate and display item

--- MJApp.js
+++ MJApp.js
@@ -1,2 +1,17 @@
+ItemList = new Mongo.Collection('blogs');
+if (Meteor.isClient) {
+  Template.body.helpers({
+  	blogs:function(){
+      	return ItemList.find();
+      }
+      
+  });  
 
-// Start Work
+  Template.registerHelper('formatDate', function(createdAt) {
+  return moment(createdAt).format('MM-DD-YYYY');
+});
+}
+
+if (Meteor.isServer) {
+  
+}
